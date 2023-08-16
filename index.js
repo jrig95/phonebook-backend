f5c43df659cbfddbbc08f770a73e76106deb56d9
James Riggleman <james.riggleman@yahoo.com>
2023-08-16 10:46:02 +0800

    finished exercise 3.6, adding error handling for name or number missing and if name already exists

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({error: 'content missing'})
+    return response.status(400).json({error: 'name or number missing'})
+  }
+
+  const nameExists = persons.some(person => person.name === body.name)
+  if (nameExists) {
+    return response.status(409).json({error: 'name already exists in the phonebook'})
   }
 
   const person = {
